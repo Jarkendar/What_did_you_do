0a8c0b7a1683030876b9a066b9d5a81c2f7708c0
Jarkendar <jarek.skrzypczak@o2.pl>
2021-06-13 19:37:58 +0200

    Add simple database

--- app/src/main/java/com/example/whatdidyoudo/ui/main/Task.kt
+++ app/src/main/java/com/example/whatdidyoudo/ui/main/Task.kt
@@ -1,5 +1,13 @@
 package com.example.whatdidyoudo.ui.main
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
-data class Task(val text: String, val timestamp: Date, val isProductive: Boolean)
+@Entity
+data class Task(
+    @PrimaryKey val id: Int,
+    val text: String,
+    val timestamp: Date,
+    val isProductive: Boolean
+)
---
+++ app/src/main/java/com/example/whatdidyoudo/ui/main/TaskDAO.kt
@@ -0,0 +1,26 @@
+package com.example.whatdidyoudo.ui.main
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDAO {
+
+    @Query("SELECT * FROM task")
+    fun getAll(): List<Task>
+
+    @Query("SELECT * FROM task WHERE task.timestamp BETWEEN :startTime AND :endTime")
+    fun getAllTaskInDay(startTime: Long, endTime: Long): List<Task>
+
+    @Insert
+    fun insertTask(task: Task)
+
+    @Update
+    fun updateTask(task: Task)
+
+    @Delete
+    fun deleteTask(task: Task)
+}
