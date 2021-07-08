d4b1e6dbd2da4878edd8f6ec90fe3442057e9319
Jarkendar <jarek.skrzypczak@o2.pl>
2021-07-08 23:41:48 +0200

    Implement navigation bottom navigation

--- app/src/main/java/com/example/whatdidyoudo/MainActivity.kt
+++ app/src/main/java/com/example/whatdidyoudo/MainActivity.kt
@@ -2,17 +2,34 @@ package com.example.whatdidyoudo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.whatdidyoudo.ui.main.TaskFragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.whatdidyoudo.databinding.MainActivityBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, TaskFragment.newInstance())
-                .commitNow()
-        }
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_bottom_navigation)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_list, R.id.navigation_statistics
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
